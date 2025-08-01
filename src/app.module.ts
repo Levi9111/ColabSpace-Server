2a79aaf805ce47e4b4680cc05712e8c711103804
levi9111 <captainlevi9111@gmail.com>
2025-08-01 21:18:41 +0600

    AppModule populated and connected with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [UsersModule],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_CONNECTION_URI')!,
+      }),
+      inject: [ConfigService],
+    }),
+
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    AuthModule,
+    UsersModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/jwt/stratigies/refresh-token.stratigy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor(private config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get<string>('JWT_REFRESH_SECRET')!,
+      passReqToCallback: true,
+    });
+  }
+
+  validate(payload: Record<string, unknown>) {
+    return payload;
+  }
+}
