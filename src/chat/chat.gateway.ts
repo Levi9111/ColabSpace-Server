24787f016e16473f612244e48c076c1e936c73f2
levi9111 <captainlevi9111@gmail.com>
2025-08-05 23:41:42 +0600

    chat gateway updatefd

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,54 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { ChatService } from './chat.service';
+import { JwtService } from '@nestjs/jwt';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  handleConnection(client: Socket) {
+    try {
+      const token = client.handshake.auth.token as string;
+
+      const user = this.jwtService.verify<{ userId: string; email: string }>(
+        token,
+      );
+
+      (client.data as { user?: any }).user = user;
+
+      console.log('User connected', user.email);
+    } catch (error) {
+      console.log('Socket connection rejected', error);
+      client.disconnect();
+    }
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(
+      'User Disconnected',
+      (client.data as { user?: any }).user?.email,
+    );
+  }
+
+  @SubscribeMessage('joinRoom')
+  async handleJoinRoom(
+    @MessageBody() roomId: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.join(roomId);
+  }
+
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
